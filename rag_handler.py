57a0018895008107dc60c395712dda394a13703c
Yusuf Bıyık <yussuf.jpeg@gmail.com>
2024-12-25 11:00:53 +0300

    Fix ollama address usage

--- rag_handler.py
+++ rag_handler.py
@@ -8,14 +8,14 @@ import argparse
 parser = argparse.ArgumentParser(description="RAG example using langchain & Chromadb")
 parser.add_argument("--model", type=str, default="llama3.2", help="Name of the model to use")
 parser.add_argument("--ingestion-folder", type=str, default="./ingest", help="Folder to ingest documents from")
-parser.add_argument("--ollama-address", type=str, default="http://127.0.0.1:11434 ", help="Ollama server address")
+parser.add_argument("--ollama-address", type=str, default="http://127.0.0.1:11434", help="Ollama server address")
 args = parser.parse_args()
 
 # Load the model // Modeli yükle
 try:
-    model = ChatOllama(model=args.model, ollama_address=args.ollama_address)
+    model = ChatOllama(model=args.model, base_url=args.ollama_address)
 except Exception as e:
-    print(f"Error loading model: {e}\n Make sure you have installed the model, if not, run:\n ollama pull model_name")
+    print(f"Error loading model: {e}\n Make sure you have installed the model and ollama is running")
 
 # Initial message // İlk mesaj
 model.invoke([SystemMessage("You are a helpful assistant that can answer questions about the given documents, only answer questions that are related to the documents if there are any documents.")])
